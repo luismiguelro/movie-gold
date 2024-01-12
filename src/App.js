2d6393f9f9907ac453d17f3c20639ca43d92bf41
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2024-01-12 10:22:13 -0500

    [feat] Hero: style the carousel

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import Layout from './components/Layout';
 import { Routes, Route } from 'react-router';
 import Home from './components/home/Home';
+
 function App() {
 
   const urlBase = "http://localhost:8080/api/v1/movies"
@@ -12,8 +13,8 @@ function App() {
   const fetchData = async () => {
     try {
       const response = await axios.get(urlBase);
-      const datos = response.data;
-      console.log(datos);
+      setMovies(response.data);
+    
 
     } catch (error) {
       console.log(error);
@@ -27,8 +28,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Layout/>}>
-          <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Layout />}>
+          <Route path='/' element={<Home movies={movies} />} />
         </Route>
       </Routes>
     </div>
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -5,14 +5,28 @@ import { Paper } from '@mui/material';
 
 const Hero = ({ movies }) => {
     return (
-        <div>
+
+        <div className='movie-carousel-container'>
 
             <Carousel>
                 {
-                    movies.map((movie) => {
-                        <Paper>
-
-                        </Paper>
+                    movies?.map((movie) => {
+                        return (
+                            <Paper key={movie.imdbId}>
+                                <div className='movie-card-container'>
+                                    <div className="movie-card" style={{ "--img": `url(${movie.backdrops[0]})` }}>
+                                        <div className="movie-detail">
+                                            <div className="movie-poster">
+                                                <img src={movie.poster} alt="" />
+                                            </div>
+                                            <div className='movie-title'>
+                                                {movie.title}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Paper>
+                        )
                     })
                 }
             </Carousel>
