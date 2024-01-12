12f3b63c0680e8a5fd68639510c330e2c9973e2b
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2024-01-12 11:47:41 -0500

    create loader component and deploy it

--- src/components/Trailer/Trailer.js
+++ src/components/Trailer/Trailer.js
@@ -2,9 +2,11 @@ import { useParams } from 'react-router-dom';
 import ReactPlayer from 'react-player';
 import './Trailer.css';
 import React, { useState, useEffect } from 'react';
+import Loader from '../constants/loader/Loader';
 
 const Trailer = () => {
   const [loading, setLoading] = useState(true);
+  const [loaderTimeoutCompleted, setLoaderTimeoutCompleted] = useState(false);
 
   let params = useParams();
   let key = params.ytTrailerId;
@@ -18,16 +20,27 @@ const Trailer = () => {
   };
 
   useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setLoading(false);
-    }, 5000);
+    const loaderTimeoutId = setTimeout(() => {
+      setLoaderTimeoutCompleted(true);
+    }, 1000); // Adjust the duration as needed (in milliseconds)
 
-    return () => clearTimeout(timeoutId);
-  }, [key]); 
+    return () => clearTimeout(loaderTimeoutId);
+  }, []); // Run the timeout effect only once
+
+  useEffect(() => {
+    if (loaderTimeoutCompleted && key) {
+      // If the loader timeout has completed and there is a video key
+      const videoTimeoutId = setTimeout(() => {
+        setLoading(false);
+      }, 2000); // Adjust the duration for the video loading (in milliseconds)
+
+      return () => clearTimeout(videoTimeoutId);
+    }
+  }, [loaderTimeoutCompleted, key]);
 
   return (
-    <div style={{height: '90vh'}}>
-      {loading && <div className="loader">Loading...</div>}
+    <div className="react-player-container">
+      {loading && loaderTimeoutCompleted && <Loader />} {/* Show the Loader after initial timeout */}
       {key != null ? (
         <ReactPlayer
           controls={true}
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,49 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Hero.css';
 import Carousel from 'react-material-ui-carousel';
 import { Paper } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircle, faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
+import Loader from '../constants/loader/Loader';
 
 const Hero = ({ movies }) => {
-    return (
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    // Simulating a loading delay
+    const timeoutId = setTimeout(() => {
+      setLoading(false);
+    }, 3000); // Simulating a 5-second loading delay
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+  return (
+    <div className='hero-container'>
+      {loading && <Loader />} {/* Show the loader while loading */}
+      {!loading && (
         <div className='movie-carousel-container'>
-
-            <Carousel>
-                {
-                    movies?.map((movie) => {
-                        return (
-                            <Paper key={movie.imdbId}>
-                                <div className='movie-card-container'>
-                                    <div className="movie-card" style={{ "--img": `url(${movie.backdrops[0]})` }}>
-                                        <div className="movie-detail">
-                                            <div className="movie-poster">
-                                                <img src={movie.poster} alt="" />
-                                            </div>
-                                            <div className='movie-title'>
-                                                <h4>{movie.title}</h4>
-                                            </div>
-                                            <div className='movie-buttons-container'>
-                                            <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
-                                                    <div className='play-button-icon-container'>
-                                                        <FontAwesomeIcon className='play-button-icon'
-                                                            icon={faCirclePlay} />
-                                                    </div>
-                                                </Link>
-
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </Paper>
-                        )
-                    })
-                }
-            </Carousel>
+          <Carousel>
+            {movies?.map((movie) => (
+              <Paper key={movie.imdbId}>
+                <div className='movie-card-container'>
+                  <div className="movie-card" style={{ "--img": `url(${movie.backdrops[0]})` }}>
+                    <div className="movie-detail">
+                      <div className="movie-poster">
+                        <img src={movie.poster} alt="" />
+                      </div>
+                      <div className='movie-title'>
+                        <h4>{movie.title}</h4>
+                      </div>
+                      <div className='movie-buttons-container'>
+                        <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
+                          <div className='play-button-icon-container'>
+                            <FontAwesomeIcon className='play-button-icon' icon={faCirclePlay} />
+                          </div>
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Paper>
+            ))}
+          </Carousel>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
