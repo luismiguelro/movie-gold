8f783bd2ca0dfc27c293b609ffe74228ec1833e7
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2024-01-17 13:21:45 -0500

    [feat] add watchlist

--- src/App.js
+++ src/App.js
@@ -7,14 +7,13 @@ import Home from './components/home/Home';
 import Header from './components/header/Header';
 import Trailer from './components/Trailer/Trailer'
 import Reviews from './components/reviews/Reviews';
-
+import WatchList from './components/watchList/WatchList';
 function App() {
 
   const urlBase = "http://localhost:8080/api/v1/movies"
   const [movies, setMovies] = useState();
   const [movie, setMovie] = useState();
   const [reviews, setReviews] = useState([]);
-
   const getMovies = async () => {
 
     try {
@@ -55,13 +54,16 @@ function App() {
   return (
     <div className="App">
       <Header />
+    
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route path='/' element={<Home movies={movies} />} />
+          <Route path='/' element={<WatchList movies={movies}/>} />
           <Route path="/trailer/:ytTrailerId" element={<Trailer />}></Route>
           <Route path="/reviews/:movieId" element={<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews} setReviews={setReviews} />}></Route>
-
+          <Route path='/watchlist' element={ <WatchList movies={movies}/>} />
         </Route>
+        
       </Routes>
     </div>
   );
---
+++ src/components/watchList/WatchList.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay, faVideo } from '@fortawesome/free-solid-svg-icons';
+
+import './WatchList.css'; // Importa tu archivo de estilo CSS
+
+function WatchList({ movies }) {
+  return (
+    <div className="card-container d-flex flex-wrap justify-content-start">
+      {movies?.map((movie, index) => (
+        <Card key={index} style={{ width: '18rem', margin: '10px' }} className="hover-card">
+          <div className="image-container">
+            <Card.Img variant="top" src={movie.poster} alt={`Card ${index + 1}`} />
+            <div className='movie-buttons-container'>
+              <Link to={`/trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`} className="hover-link" style={{ textDecoration: "none" }}>
+                <div className='play-button-icon-container'>
+                  <FontAwesomeIcon className='play-button-icon' icon={faCirclePlay} />
+                  <span style={{ color: "white", textDecoration: "none" }}>Watch Trailer</span>
+                </div>
+              </Link>
+              <Link to={movie.movieLink} target='_blank' className="hover-link" style={{ textDecoration: "none" }}>
+                <div className='play-button-icon-container'>
+                  <FontAwesomeIcon className='play-button-icon' icon={faVideo} />
+                  <span style={{ color: "white", textDecoration: "none" }}>Watch Movie</span>
+                </div>
+              </Link>
+            </div>
+          </div>
+          <Card.Body>
+            <Card.Title>{movie.title}</Card.Title>
+            <Card.Text>{new Date(movie.releasemovie).getFullYear()}</Card.Text>
+          </Card.Body>
+        </Card>
+      ))}
+    </div>
+  );
+}
+
+export default WatchList;
