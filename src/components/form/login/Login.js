a8fd45a2f7d3ad7e0b1a4a25659948975d51f184
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2024-04-11 22:48:29 -0500

    [feat] successful login and fields requiring authentication

--- src/components/form/login/Login.js
+++ src/components/form/login/Login.js
@@ -1,56 +1,54 @@
 import React, { useState } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
-import axiosInstance from '../../api/axios';
-import { jwtDecode } from 'jwt-decode' // import dependency
+import useAuthFunctions from '../functions/useAuthFunction';
+import { Form, Button, Alert } from 'react-bootstrap'; // Importa los componentes necesarios
+import '../register/Register.css'; // Importa los estilos CSS del registro aquí
 
-
-const LoginForm = ({ onLogin,  setUser }) => {
+const LoginForm = ({ setUser }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
+    const { handleSubmit, error } = useAuthFunctions();
 
-    const handleSubmit = async (e) => {
+    const handleFormSubmit = (e) => {
         e.preventDefault();
-
-        try {
-            axiosInstance.post('/api/v1/auth/authenticate', {
-                email: email.toLowerCase(),
-                password: password
-            })
-            .then(res =>{
-                const token = res.data.token;
-                const user = jwtDecode(token); // decode token
-                console.log(user);
-                setUser(user)
-            })
-
-           
-        } catch (error) {
-            // Handle request errors
-        
-            setError(error.response);
-        }
+        handleSubmit(email, password, setUser);
     };
 
     return (
-        <div>
-            {error && <Alert variant="danger">{error}</Alert>}
-            <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Email address" value={email} onChange={(e) => setEmail(e.target.value)} />
-                </Form.Group>
+        <section className="text-center">
+            {/* Background image */}
+            <div className="p-5 bg-image" style={{ backgroundImage: "url('https://mdbootstrap.com/img/new/textures/full/171.jpg')", height: '300px' }}></div>
+            {/* Background image */}
 
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                </Form.Group>
+            <div className="card mx-4 mx-md-5 shadow-5-strong" style={{ marginTop: '-100px', background: 'hsla(0, 0%, 100%, 0.8)', backdropFilter: 'blur(30px)' }}>
+                <div className="card-body py-5 px-md-5">
 
-                <Button variant="primary" type="submit">
-                    Login
-                </Button>
-            </Form>
-        </div>
+                    <div className="row d-flex justify-content-center">
+                        <div className="col-lg-8">
+                            <h2 className="fw-bold mb-5">Login now</h2>
+                            {error && <Alert variant="danger">{error}</Alert>}
+                            <Form onSubmit={handleFormSubmit}>
+                                <div className="row">
+                                    <div className="col-md-6 mb-4">
+                                        <div data-mdb-input-init className="form-outline">
+                                            <input type="email" id="formBasicEmail" className="form-control" placeholder="Email address" value={email} onChange={(e) => setEmail(e.target.value)} />
+                                            <label className="form-label" htmlFor="formBasicEmail">Email</label>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6 mb-4">
+                                        <div data-mdb-input-init className="form-outline">
+                                            <input type="password" id="formBasicPassword" className="form-control" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                                            <label className="form-label" htmlFor="formBasicPassword">Password</label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <Button type="submit" variant="primary" className="btn btn-block mb-4">Login</Button>
+                            </Form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
